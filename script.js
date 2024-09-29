d77b544684d1547bcb2fc42e10891ab0152bb5f6
darky9197 <86095469+darky9197@users.noreply.github.com>
2024-09-29 10:25:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function run(){
+    const ios = document.getElementById('ios').value
+    const toc = document.getElementById('toc').value
+    const oslab = document.getElementById('oslab').value
+    const dbmslab = document.getElementById('dbmslab').value
+    const aiml = document.getElementById('aiml').value
+    const dbms = document.getElementById('dbms').value
+    const ess = document.getElementById('ess').value
+    const we = document.getElementById('we').value
+    const nmlab = document.getElementById('ios').value
+
+    let gpa = ((Number(ios)*3)+(Number(toc)*3)+(Number(oslab)*1.5)+(Number(dbmslab)*1.5)+(Number(aiml)*4)+(Number(dbms)*3)+(Number(ess)*2)+(Number(we)*4)+(Number(nmlab)*1.5)) / (23.5)
+
+
+    console.log(gpa.toFixed(2));
+    console.log(typeof(Number(ios.value)));
+    const output = document.getElementById('output').textContent = gpa.toFixed(2)
+}
