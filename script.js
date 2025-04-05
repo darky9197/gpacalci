4219ce64c8c240b66ee7a25390897ca9447fc9cc
darkky9197 <darkking9197@gmail.com>
2025-04-05 20:12:08 +0530

    script update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function add(arr){
+function sem4(arr){
     res = 0;
     arr.forEach(element => {
         res = res + element
@@ -23,6 +23,35 @@ function run(){
     console.log(ios*3);
     console.log(we*4);
     let arr = [(ios*3),(toc*3),(oslab*1.5),(dbmslab*1.5),(aiml*4),(dbms*3),(ess*2),(we*4),(nmlab*2)]
+    console.log(arr);
+    let gpa = add(arr) / 24
+
+    const output = document.getElementById('output').textContent = `GPA : ${gpa.toFixed(2)}`
+}
+
+
+function sem5(arr){
+    res = 0;
+    arr.forEach(element => {
+        res = res + element
+    });
+
+    return res
+}
+
+function run(){
+    const devops = document.getElementById('devops').value
+    const ba = document.getElementById('ba').value
+    const dc = document.getElementById('dc').value
+    const cn = document.getElementById('cn').value
+    const esiot = document.getElementById('esiot').value
+    const fswd = document.getElementById('fswd').value
+    const fswdlab = document.getElementById('fswdlab').value
+    const drrm = document.getElementById('drrm').value
+    const nmlab = document.getElementById('nmlab').value
+
+
+    let arr = [(devops*3),(ba*3),(dc*3),(cn*4),(esiot*4),(fswd*3),(fswdlab*2),(drrm*0),(nmlab*2)]
     console.log(arr);
     let gpa = add(arr) / 24
 
